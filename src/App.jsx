0330cd5486e9121123a546174c34be2f60a31c87
Nathan Cruz <103093320+NateCruz@users.noreply.github.com>
2025-12-03 12:06:09 -1000

    fixed path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Homepage from "./src/pages/Homepage";
+import Homepage from "./src/pages/Homepage.jsx";
 
 function App() {
   return (
